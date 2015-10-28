7b93d032c8efae7ded7fc26d86b4c2ecf9ecfc9c
Thibault Sellam <sellam@cwi.nl>
2015-10-28 14:47:25 +0100

    Introduced bivariate z-components

--- Code/Ziggy.R
+++ Code/Ziggy.R
@@ -2,40 +2,77 @@ library(foreign)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+#library(compiler)
 
 MIN_SPARSITY <- 0.25
 
 data <- read.arff("~/Data/Files/crime/communities.arff")
-for (j in 10:20) data[[j]] <- as.character(cut(data[[j]], 10))
+data_check <- data
+#data <- rbind(data, data, data, data, data)
+
+for (j in 20:50)
+    data[[j]] <- as.character(cut(data[[j]], 3))
 selection <- data$ViolentCrimesPerPop > 0.7
 
-check_group <- function(col_set){
+check_group <- function(col_set, dataset = data){
+
+   types <- sapply(col_set, function(col) class(dataset[,col] ))
+   cat("Ploting for types:")
+   print(types)
 
    if (length(col_set) == 1){
 
-      if (class(data[[col_set]]) == "numeric"){
-         g <- ggplot(data, aes_string(x = col_set,
+      if (types == "numeric"){
+         g <- ggplot(dataset, aes_string(x = col_set,
                                  fill='selection', color = 'selection')) +
               geom_histogram(position = "dodge", aes(y = ..density..))
          print(g)
 
          cat("Baseline mean equality test:\n")
-         print(t.test(data[selection, col_set], data[!selection, col_set]))
+         print(t.test(dataset[selection, col_set], dataset[!selection, col_set]))
 
          cat("Baseline variance ratio test:\n")
-         print(var.test(data[selection, col_set], data[!selection, col_set]))
+         print(var.test(dataset[selection, col_set], dataset[!selection, col_set]))
       }
 
-      if (class(data[[col_set]]) == "factor"){
+      if (types == "factor"){
 
-         g <- ggplot(data, aes_string(x = col_set,
+         g <- ggplot(dataset, aes_string(x = col_set,
                                       fill='selection', color = 'selection')) +
             geom_histogram(position = "dodge", aes(y = ..count../sum(..count..)))
          print(g)
 
-         tab <- table(data[selection, col_set])
-         ps <- table(data[!selection, col_set]) / sum(table(data[!selection, col_set]))
+         tab <- table(dataset[selection, col_set])
+         ps <- table(dataset[!selection, col_set]) / sum(table(dataset[!selection, col_set]))
          print(chisq.test(tab, p =  ps))
+      }
+
+   }
+
+   if (length(col_set) == 2){
+
+      if (all(types == "numeric")){
+
+         g <- ggplot(dataset, aes_string(x = col_set[1],
+                                      y = col_set[2],
+                                      fill = 'selection',
+                                      color = 'selection')) +
+            geom_point() +
+            geom_smooth(method = lm)
+
+         print(g)
+
+      }
+
+      if (all(types == "factor")){
+
+         dataset$selection <- factor(selection)
+         g <- ggplot(dataset, aes_string(x = col_set[1], y = col_set[2])) +
+            geom_bin2d() +
+            facet_wrap( ~ selection)
+
+         print(g)
+
       }
 
    }
@@ -45,7 +82,7 @@ check_group <- function(col_set){
 #############################
 # Reading and preprocessing #
 #############################
-preprocess <- function(data, nbins=10){
+preprocess <- function(data, nbins=4){
     cat("Preprocessing in progress...")
     types <- sapply(data, class)
 
@@ -109,6 +146,7 @@ compute_uni_stats <- function(data, exclude_cols = c()){
    cat("Computing univariate stats for categorical values\n")
    tru_cat_cols <- cat_cols[!grepl('!!', cat_cols)]
    histograms   <- lapply(data[,tru_cat_cols], function(col) table(col))
+   #histograms   <- lapply(tru_cat_cols, function(col) bigtable(data, col))
    stats_cat_uni <- data_frame(column = tru_cat_cols,
                                   hist   = histograms)
 
@@ -164,9 +202,7 @@ rollback_cat_uni <- function(all_cat_uni, sel_cat_uni){
 
 # Computes dissimilarities for univariate numerical values
 mean_magnitude <- function(m1, m2, v1, v2, n1, n2){
-   p_s <- sqrt(  (n1 - 1)*v1 + (n2 - 1)*v2 / (n1 + n2 - 2) )
-   # Alternative: p_s <- sqrt(v2)
-   abs((m1 - m2) / sqrt(p_s))
+   (m1 - m2) / sqrt(v2)
 }
 
 mean_test <- function(mean1, mean2, var1, var2, n1, n2){
@@ -176,7 +212,7 @@ mean_test <- function(mean1, mean2, var1, var2, n1, n2){
 }
 
 variance_magnitude <- function(var1, var2)
-   1 - sqrt(pmin(var1, var2)) / sqrt(pmax(var1, var2))
+   sqrt(var2/var1) - 1
 
 variance_test <- function(var1, var2, n1, n2){
    # Making sure than var1 <= var2
@@ -231,7 +267,7 @@ scores_num_uni <- function(sel_num_uni, exc_num_uni){
 cat_single_test_magn <- function(hists1, hists2){
 
    # In this case, hists2 describe the expected distributions
-   sapply(1:length(hists2), function(i, hists1, hists2){
+   out <- sapply(1:length(hists2), function(i, hists1, hists2){
       hist1 <- hists1[[i]]
       hist2 <- hists2[[i]]
 
@@ -245,9 +281,21 @@ cat_single_test_magn <- function(hists1, hists2){
       # Cramer's V... or at least I hope :)
       M <- sqrt( X2 / ((length(hist2) - 1) *  sum(hist1)))
 
-      c('p_chi2_test' = P, 'm_chi2_test' = M)
+      c('p_chi2_test' = P, 'm_chi2_test' = M, 'X2'=X2)
 
    }, hists1, hists2)
+
+#    checks <- sapply(1:length(hists2), function(i, hists1, hists2){
+#       hist1 <- hists1[[i]]
+#       hist2 <- hists2[[i]]
+#
+#       P <- hist2 / sum(hist2)
+#       out <- chisq.test(hist1, p=P)
+#       c('p_check' = out$p.value, 'X_check' = out$statistic)
+#    }, hists1, hists2)
+#    browser()
+
+   out
 }
 
 scores_cat_uni <- function(sel_cat_uni, exc_cat_uni){
@@ -270,6 +318,51 @@ scores_cat_uni <- function(sel_cat_uni, exc_cat_uni){
 #------------------------------#
 # Single Population Statistics #
 #------------------------------#
+o_calc_cramer_V <- function(cross_tabs){
+   sapply(cross_tabs, function(tab){
+
+      # First, filter our empty rows/columns
+      null_rows <- apply(tab, 1, sum) == 0
+      null_cols <- apply(tab, 2, sum) == 0
+      tab <- tab[!null_rows, !null_cols, drop=FALSE]
+
+      # Checks trivial cases
+      V_norm <- min(nrow(tab) - 1, ncol(tab) - 1)
+      total  <- sum(tab)
+      if (V_norm < 1 | total < 10)
+         return(
+            c('p_chi2'    = NA,
+              'Chi_chi2'  = NA,
+              'm_CramersV'= NA,
+              'df'        = NA,
+              'V_norm'    = NA,
+              'count'     = NA
+            )
+         )
+
+      # Computes the actual stats
+      res <- chisq.test(tab)
+      V   <- sqrt(res$statistic / (total * V_norm))
+
+      # Checks Chi-squared assumptions
+       p_chi2 <- if (sum(tab < 5) > 0.3*length(tab)){
+          NA
+       } else {
+          res$p.value
+       }
+
+      c('p_chi2'    = p_chi2,
+        'Chi_chi2'  = res$statistic,
+        'm_CramersV'= V,
+        'df'        = res$parameter,
+        'V_norm'    = V_norm,
+        'count'     = total
+        )
+   })
+}
+#calc_cramer_V <- cmpfun(o_calc_cramer_V, c('optimize'=3))
+calc_cramer_V <- o_calc_cramer_V
+
 compute_bi_stats <- function(data, uni_stats, exclude_cols = NULL){
    # Simple sanity checks
    if (!is.data.frame(data))
@@ -339,13 +432,18 @@ compute_bi_stats <- function(data, uni_stats, exclude_cols = NULL){
 
    # Formatting
    count_tables <- count_tables[!sapply(count_tables, is.null)]
-   count_tables <- unlist(count_tables, recursive = FALSE)
+   count_tables <- unlist(count_tables, recursive = F)
    col_names <- sapply(count_tables, function(tab){
       names(attributes(tab)$dimnames)
    })
    stats_cat_bi <- data.frame(t(col_names), stringsAsFactors = F)
    names(stats_cat_bi) <- c('column2', 'column1')
-   stats_cat_bi$cross_tabs <- count_tables
+   stats_cat_bi[['cross_tabs']] <- count_tables
+
+   # Augments with Chi-2 tests
+   add_stats <- t(calc_cramer_V(count_tables))
+   colnames(add_stats) <- c('p_Chi2', 'Chi2', 'CramersV', 'df', 'V_norm_fact', 'count')
+   stats_cat_bi <- cbind(stats_cat_bi, add_stats)
 
    return(list(
        stats_num_bi = stats_num_bi,
@@ -382,12 +480,6 @@ rollback_num_bi  <- function(all_num_bi, sel_num_bi,
                    mutate(exc_count = pmin(col1_exc_count, col2_exc_count)) %>%
                    select(-col1_exc_count, -col2_exc_count)
 
-    check_cv <- function(cols1, cols2){
-       sapply(1:length(cols1), function(j){
-            cov(data[!selection, cols1[j]], data[!selection, cols2[j]])
-       })
-    }
-
     exc_num_bi <- exc_num_bi %>%
                    mutate(C_all = (sel_count + exc_count - 1) * all_covariance,
                           C_sel = (sel_count - 1) * sel_covariance,
@@ -401,14 +493,11 @@ rollback_num_bi  <- function(all_num_bi, sel_num_bi,
                           covariance,
                           col1_variance = col1_exc_variance,
                           col2_variance = col2_exc_variance,
-                          count = exc_count) %>%
-                  mutate(check = check_cv(column1, column2))
-
+                          count = exc_count)
    return(exc_num_bi)
 }
 
 rollback_cat_bi <- function(all_cat_bi, sel_cat_bi){
-
    cat("Un-updates preprocessed bivariate categorical statistics\n")
 
    exc_cat_bi <- sel_cat_bi %>%
@@ -419,7 +508,76 @@ rollback_cat_bi <- function(all_cat_bi, sel_cat_bi){
              summarise(column1, column2,
                        cross_tabs = list(all_cross_tabs - sel_cross_tabs))
 
+   # Augments with Chi-2 tests
+   add_stats <- t(calc_cramer_V(exc_cat_bi$cross_tabs))
+   colnames(add_stats) <- c('p_Chi2', 'Chi2', 'CramersV', 'df',
+                            'V_norm_fact', 'count')
+   exc_cat_bi <- cbind(exc_cat_bi, add_stats)
+
    return(exc_cat_bi)
+}
+
+scores_num_bi <- function(sel_num_bi, exc_num_bi){
+
+   cat("Computing two-population bivariate tests, numerical data\n")
+   stats <- sel_num_bi %>%
+                   inner_join(exc_num_bi, c('column1', 'column2'))
+
+   # Effect size
+   stats <- stats %>%
+      mutate(sel_corr = covariance.x /
+                (sqrt(col1_variance.x * col2_variance.x)),
+             exc_corr = covariance.y /
+                (sqrt(col1_variance.y * col2_variance.y)),
+             m_corr_diff = sel_corr - exc_corr)
+
+   # Testing
+   stats <- stats %>%
+            mutate(sel_z = atanh(sel_corr),
+                   exc_z = atanh(exc_corr),
+                   sel_z_var = ifelse(count.x > 10, 1 / (count.x - 3), NA),
+                   exc_z_var = ifelse(count.y > 10, 1 / (count.y - 3), NA)) %>%
+            mutate(t_corr_sel = 2 * pnorm(-abs(sel_z / sqrt(sel_z_var))),
+                   t_corr_exc = 2 * pnorm(-abs(exc_z / sqrt(exc_z_var))),
+                   test_z_diff = (sel_z - exc_z) /
+                              sqrt( sel_z_var + exc_z_var ),
+                   t_corr_diff = 2 * pnorm(-abs(test_z_diff)))
+
+   stats <- select(stats, column1, column2,
+                   sel_corr, exc_corr, t_corr_sel, t_corr_exc,
+                   m_corr_diff, t_corr_diff)
+
+}
+
+
+test_Chi2_ratio <- function(V_sel, V_exc, df_sel, df_exc,
+                            fact_sel, fact_exc,
+                            count_sel, count_exc){
+
+   # Fisher / Wilson & Hilferty transform
+   mean_z_sel <- sqrt((df_sel - 0.5)/(fact_sel * count_sel))
+   mean_z_exc <- sqrt((df_exc - 0.5)/(fact_exc * count_exc))
+   var_z_sel <- 1 / (2 * fact_sel * count_sel)
+   var_z_exc <- 1 / (2 * fact_exc * count_exc)
+
+   Z <- ((V_sel - V_exc) - (mean_z_sel - mean_z_exc)) /
+      sqrt(var_z_sel + var_z_exc)
+
+   P <- 2*pnorm(-abs(Z))
+}
+
+scores_cat_bi <- function(sel_cat_bi, exc_cat_bi){
+
+   cat("Computing two-population bivariate tests, categorical data\n")
+   stats <- sel_cat_bi %>%
+            inner_join(exc_cat_bi, c('column1', 'column2'))
+
+   stats <- mutate(stats, m_CramersV_diff = CramersV.x - CramersV.y,
+                          t_CramersV_diff = test_Chi2_ratio(
+                                                CramersV.x, CramersV.y,
+                                                df.x, df.y,
+                                                V_norm_fact.x, V_norm_fact.y,
+                                                count.x, count.y))
 }
 
 #########################
@@ -457,14 +615,15 @@ comment <- function(selection, data, offline_uni_stats, offline_bi_stats){
    exc_num_uni <- rollback_num_uni(all_num_uni, sel_num_uni)
    exc_cat_uni <- rollback_cat_uni(all_cat_uni, sel_cat_uni)
 
-   # Computes zig-scores
+   # Computes zig-components
    zig_num_uni <- scores_num_uni(sel_num_uni, exc_num_uni)
    zig_cat_uni <- scores_cat_uni(sel_cat_uni, exc_cat_uni)
 
 
    # Bivariate statistics
-   all_num_bi<- offline_bi_stats$stats_num_bi
-   all_cat_bi  <- offline_bi_stats$stats_cat_bi
+   all_num_bi  <- offline_bi_stats$stats_num_bi
+   all_cat_bi  <- offline_bi_stats$stats_cat_bi %>%
+                  filter(is.finite(p_Chi2))
 
    cat("* Bivariate statistics....\n")
    sel_bi_stats <- compute_bi_stats(sel_data,
@@ -472,15 +631,25 @@ comment <- function(selection, data, offline_uni_stats, offline_bi_stats){
                                     exclude_cols = sparse_columns)
    sel_num_bi <- sel_bi_stats$stats_num_bi
    sel_cat_bi <- sel_bi_stats$stats_cat_bi
+   # Removes useless entries
+   sel_cat_bi <- filter(sel_cat_bi, is.finite(Chi2), is.finite(p_Chi2))
 
-   # Un-unpdate the full table statistics
+   # Un-updates the full table statistics
    exc_num_bi <- rollback_num_bi(all_num_bi, sel_num_bi,
                                  sel_num_uni, exc_num_uni)
    exc_cat_bi <- rollback_cat_bi(all_cat_bi, sel_cat_bi)
+
+   # Computes zig-components
+   zig_num_bi <- scores_num_bi(sel_num_bi, exc_num_bi)
+   zig_cat_bi <- scores_cat_bi(sel_cat_bi, exc_cat_bi)
+
 }
 
 # Workflow
 data  <- preprocess(data)
+CLOCK1 <- proc.time()['elapsed']
 offline_uni_stats   <- compute_uni_stats(data)
 offline_bi_stats <- compute_bi_stats(data, offline_uni_stats)
 comments <- comment(selection, data, offline_uni_stats, offline_bi_stats)
+CLOCK2 <- proc.time()['elapsed']
+print(CLOCK2 - CLOCK1)
