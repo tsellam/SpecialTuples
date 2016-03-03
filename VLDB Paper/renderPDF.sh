4f66dbd5d4105f8cef5816825f5a949e634990fe
Thibault Sellam <sellam@cwi.nl>
2016-03-03 10:06:55 +0100

    Added VLDB paper

---
+++ VLDB Paper/renderPDF.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+MAINFILE=MME
+
+REFSIZE=''
+
+rm -f .pdflatex.log .bibtex.log
+echo pdflatex.1 && pdflatex $MAINFILE              </dev/null > .pdflatex.log && \
+echo bibtex.1   && bibtex   $MAINFILE              </dev/null >   .bibtex.log && \
+sed -i 's|^\(\\begin{thebibliography}{.*}\)$|\1\n\n'$REFSIZE'|' $MAINFILE.bbl && \
+echo pdflatex.2 && pdflatex $MAINFILE              </dev/null > .pdflatex.log && \
+echo bibtex.2   && bibtex   $MAINFILE              </dev/null >   .bibtex.log && \
+sed -i 's|^\(\\begin{thebibliography}{.*}\)$|\1\n\n'$REFSIZE'|' $MAINFILE.bbl && \
+echo pdflatex.3 && pdflatex $MAINFILE              </dev/null > .pdflatex.log && \
+echo            && egrep --color -i 'warning|error|written'     .pdflatex.log || \
+cat .pdflatex.log 2>/dev/null && echo
+cat   .bibtex.log 2>/dev/null && echo
+
+cp MME.pdf 721.pdf
